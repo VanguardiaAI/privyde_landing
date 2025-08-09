7eddf46d8276e7d5aaf374e36ad304aab84fa798
Pablo Luque <tu-email@ejemplo.com>
2025-08-09 11:33:11 -0600

    Reduce spacing between separator line and ALIANZAS ESTRATÉGICAS title
    - Reduced section padding from py-16/py-24 to py-8/py-12
    - Better visual flow with less empty space above the title
    - Consistent spacing throughout the page

--- src/pages/Companies.tsx
+++ src/pages/Companies.tsx
@@ -649,7 +649,7 @@ const Companies = () => {
       </section>
 
       {/* Strategic Partnerships Section */}
-      <section className="bg-black py-16 md:py-24">
+      <section className="bg-black py-8 md:py-12">
         <div className="container mx-auto px-4">
           {/* Title */}
           <h2
